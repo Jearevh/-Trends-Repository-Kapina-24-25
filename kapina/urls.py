3d25dceea8f7170747c38be6bfe784d7e187c104
Jearevh <jenatividad@gbox.adnu.edu.ph>
2025-05-10 20:50:07 +0800

    Added htmls for main features, removed some

--- kapina/urls.py
+++ kapina/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing, name='landing'),
     path('inventory/', views.inventory, name='inventory'),
+    path('add-product/', views.add_product, name='addproduct'),
+    path('shop/', views.shop_page, name='shop_page'),
+    path('users/', views.users, name='users'),
 ]
 
--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,12 @@ def inventory(request):
 
 def landing(request):
     return render(request, 'user/landing.html')
+    
+def add_product(request):
+    return render(request, 'admins/addproduct.html')
+
+def shop_page(request):
+    return render(request, 'admins/shop_page.html')
+
+def users(request):
+    return render(request, 'admins/users.html')
